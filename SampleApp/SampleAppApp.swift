8b948950c12ef1ee86c7885e351ad4f818a05dcc
Trevor Roberts <trevorr@amazon.com>
2021-10-27 10:43:48 -0400

    initial commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Weyerhaeuser, Markus on 12.10.21.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
